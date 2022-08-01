9048ef596fe75f51a1d4d7d7e3c235717eac3c44
Karol Skolasiński <karol.skolasinski@7willows.com>
2022-08-01 19:38:43 +0200

    footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import './Footer.css';
+import github from '../../img/github-original.svg';
+import linkedin from '../../img/linkedin-original.svg';
 
 const Footer = () => (
     <footer className="footer">
-        2022 © Created by: <a href={'https://github.com/karolskolasinski'}>karolskolasinski</a>
+        <span>2022 © Created by: <a href={'https://github.com/karolskolasinski'}>karolskolasinski</a></span>
+        <div className={'footer-img-wrapper'}>
+            <a href={'https://github.com/karolskolasinski'} title={'GitHub'}>
+                <img src={github} alt={'github'} className={'footer-img'} />
+            </a>
+
+            <a href={'https://www.linkedin.com/in/karolskolasinski/'} title={'LinkedIn'}>
+                <img src={linkedin} alt={'linkedin'} className={'footer-img'} />
+            </a>
+        </div>
     </footer>
 );
 
