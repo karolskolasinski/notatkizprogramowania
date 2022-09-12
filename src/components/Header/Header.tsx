e03a384aa224eb14bea2e84645b9be520143e308
Karol Skolasiński <karol.skolasinski@7willows.com>
2022-09-12 07:15:42 +0200

    style

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,13 +6,11 @@ const Header = () => {
     return (
         <>
             <header className={'header'}>
-                <Link to={'/'} className={'header-title'}>
-                    notatki
-                    <span className={'header-accent'}>z</span>
-                    programowania</Link>
                 <nav>
-                    <Link to="/">INDEX</Link>
-                    <Link to="/html-category">HTML</Link>
+                    <Link to={'/'} className={'header-title'}>
+                        notatki
+                        <span className={'header-accent'}>z</span>
+                        programowania</Link>
                 </nav>
             </header>
 
