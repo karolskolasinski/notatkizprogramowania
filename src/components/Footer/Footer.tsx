36ae412091f981e316194c3d521bafbed9860540
Karol Skolasiński <karol.skolasinski@7willows.com>
2022-09-12 19:46:47 +0200

    responsive

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,16 +5,18 @@ import linkedin from '../../img/linkedin-original.svg';
 
 const Footer = () => (
     <footer className="footer">
-        <span>2022 © Created by: <a href={'https://github.com/karolskolasinski'}>karolskolasinski</a></span>
-        <div className={'footer-img-wrapper'}>
-            <a href={'https://github.com/karolskolasinski'} title={'GitHub'}>
-                <img src={github} alt={'github'} className={'footer-img'} />
-            </a>
+        <section>
+            <span>2022 © Created by: <a href={'https://github.com/karolskolasinski'}>karolskolasinski</a></span>
+            <div className={'footer-img-wrapper'}>
+                <a href={'https://github.com/karolskolasinski'} title={'GitHub'}>
+                    <img src={github} alt={'github'} className={'footer-img'} />
+                </a>
 
-            <a href={'https://www.linkedin.com/in/karolskolasinski/'} title={'LinkedIn'}>
-                <img src={linkedin} alt={'linkedin'} className={'footer-img'} />
-            </a>
-        </div>
+                <a href={'https://www.linkedin.com/in/karolskolasinski/'} title={'LinkedIn'}>
+                    <img src={linkedin} alt={'linkedin'} className={'footer-img'} />
+                </a>
+            </div>
+        </section>
     </footer>
 );
 
