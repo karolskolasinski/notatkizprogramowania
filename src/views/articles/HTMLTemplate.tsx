1a2626e7aae353a1035c0a36dfec3af3b538acf0
Karol Skolasiński <karol.skolasinski@7willows.com>
2022-11-06 20:29:02 +0100

    style links

--- src/views/articles/HTMLTemplate.tsx
+++ src/views/articles/HTMLTemplate.tsx
@@ -7,14 +7,18 @@ const HTMLTemplate = () => {
     return (
         <>
             <div className={'cover-wrapper'}>
-                <img src={cover} alt={'html cover'} className={'cover'}/>
+                <img src={cover} alt={'html cover'} className={'cover'} />
             </div>
             <article className={'article'}>
                 <h1>Semantic HTML</h1>
                 <p>A semantic element clearly describes its meaning to both the browser and the developer.</p>
                 <div className="article-img-wrapper">
-                    <img src={template} alt={'html template'} className={'article-img'}/>
+                    <img src={template} alt={'html template'} className={'article-img'} />
                 </div>
+                <h4>Źródło:</h4>
+                <a href={'https://internetingishard.netlify.app/html-and-css/semantic-html/index.html#summary'}
+                    className={'article-link'}>
+                    https://internetingishard.netlify.app/html-and-css/semantic-html/index.html#summary</a>
             </article>
         </>
     );
