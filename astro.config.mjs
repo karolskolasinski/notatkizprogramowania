9a05f76a48f59b08f5477505c980975ac30fec9a
Karol Skolasiński <kskolasinski@man.poznan.pl>
2026-06-21 15:30:06 +0200

    update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,6 +11,7 @@ const env = loadEnv("", process.cwd(), "PUBLIC_");
 
 export default defineConfig({
   site: env.PUBLIC_SITE_URL,
+  output: process.env.NODE_ENV === "production" ? "server" : "static",
 
   redirects: {
     "/admin": "/keystatic",
